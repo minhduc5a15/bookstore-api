9459ecd0e580fcd142de88b0e2b6f434b8d89d04
minhduc5a15 <minhduc5a15@gmail.com>
2024-11-11 09:24:05 +0700

    fix book route

--- src/routes/book.route.ts
+++ src/routes/book.route.ts
@@ -12,4 +12,24 @@ router.get('/books', async (req, res) => {
     return res.json(books);
 });
 
+// [GET] /api/books/:bookId
+router.get('/books/:bookId', async (req, res) => {
+    const db = await getDb();
+    if (!db) {
+        return res.status(500).send('Error connecting to database');
+    }
+    try {
+        const book = await db.collection('books').findOne({ id: req.params.bookId });
+
+        if (!book) {
+            return res.status(404).json({ message: 'Book not found' });
+        }
+
+        return res.json(book);
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: 'Error getting book', error });
+    }
+});
+
 export default router;
